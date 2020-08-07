9fabe319c46a08c0b066cfb265f0acdcb1a8d5db
TangZhongyi <ztan9766@outlook.com>
2020-08-07 14:13:26 +0800

    add demo & component

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # uni-simple-address
+根据uniapp插件作者[SimpleJalon](https://ext.dcloud.net.cn/publisher?id=44551)的[三级地址联动，支持（app）nvue、小程序、H5](https://ext.dcloud.net.cn/plugin?id=1084)插件替换了地区数据源，使用了[基于 puppeteer 的中国行政区域抓取爬虫](https://github.com/dwqs/area-puppeteer)的格式化数据文件pcaa.js替换了原来的province.js/city.js/area.js三个文件。会比原插件体积上小一点点。
+
+替换原因主要为需要和后台系统中已经使用的VUE省市区组件数据同步，所以引用了同一个地区数据源
+
+## 使用方式
+
+直接复制代码中simple-address文件夹，作为component放在components下使用
+
+## 安装
+```
+yarn install
+```
+
+### 运行DEMO
+```
+yarn serve
+```
---
+++ src/pages/index/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <view class="content">
+    <view @tap="onAddressOpen">
+      {{ address.label || '您的城市' }}
+    </view>
+
+    <simple-address ref="simpleAddress" :pickerValueDefault="addressIndex" @onConfirm="onAddressConfirm">
+    </simple-address>
+  </view>
+</template>
+
+<script>
+import SimpleAddress from '../../../simple-address/simple-address'
+
+export default {
+  name: 'Index',
+  components: {
+    SimpleAddress,
+  },
+  data() {
+    return {
+      title: 'Hello',
+      address: {},
+      addressIndex: [0, 0, 0],
+    }
+  },
+  onLoad() {},
+  methods: {
+    onAddressOpen() {
+      this.$refs.simpleAddress.open()
+    },
+    onAddressConfirm(e) {
+      console.log(e)
+      this.address = e
+    },
+  },
+}
+</script>
+
+<style>
+.content {
+  padding: 30rpx;
+  margin: 30rpx;
+  text-align: center;
+  border: 1px solid;
+  border-radius: 8rpx;
+}
+</style>
